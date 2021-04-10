f4a98531037c2dc8ca55e3699e1160f04145fa2a
Brandon Knox <bknox.dev@gmail.com>
2021-04-10 13:59:23 -0700

    Initial Commit

---
+++ DrawingChallenge/DrawingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingChallengeApp.swift
+//  DrawingChallenge
+//
+//  Created by Brandon Knox on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
