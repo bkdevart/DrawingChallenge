85741f0f59c26372529bd6422f85c3383f610f14
Brandon Knox <bknox.dev@gmail.com>
2021-04-10 16:19:49 -0700

    Combined paths into single shape.  Modified X values by multiplying 0.25 or 0.75 where appropriate

--- DrawingChallenge/ContentView.swift
+++ DrawingChallenge/ContentView.swift
@@ -7,27 +7,19 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
+struct Arrow: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
         path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
         path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
         
-        return path
-    }
-}
-
-struct Rectangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.move(to: CGPoint(x: rect.maxX * 0.25, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.maxX * 0.25, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX * 0.75, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX * 0.75, y: rect.midY))
         
         return path
     }
@@ -36,12 +28,8 @@ struct Rectangle: Shape {
 struct ContentView: View {
     var body: some View {
         VStack {
-            Triangle()
-                .fill(Color.green)
-                .frame(width:300, height: 300)
-            Rectangle()
-                .fill(Color.green)
-                .frame(width:100, height: 300)
+            Arrow()
+                .frame(width:200, height: 300)
         }
     }
 }
