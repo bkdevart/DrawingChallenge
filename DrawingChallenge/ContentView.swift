5d3e8c8f9f60ff487f23af634498e9da930d8712
Brandon Knox <bknox.dev@gmail.com>
2021-04-10 16:47:53 -0700

    Changed arrow to stroke only (no fill), added slider to animate thickness

--- DrawingChallenge/ContentView.swift
+++ DrawingChallenge/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct Arrow: Shape {
+//    var lineWidth: CGFloat
+//
+//    var animatableData: CGFloat {
+//        get { lineWidth }
+//        set { self.lineWidth = newValue }
+//    }
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
@@ -15,6 +22,7 @@ struct Arrow: Shape {
         path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
         path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
         path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
         
         path.move(to: CGPoint(x: rect.maxX * 0.25, y: rect.midY))
         path.addLine(to: CGPoint(x: rect.maxX * 0.25, y: rect.maxY))
@@ -26,11 +34,18 @@ struct Arrow: Shape {
 }
 
 struct ContentView: View {
+    @State private var lineWidth: CGFloat = 5.0
+    
     var body: some View {
         VStack {
             Arrow()
+                .stroke(Color.green, lineWidth: lineWidth)
                 .frame(width:200, height: 300)
+            Slider(value: $lineWidth, in: 0...100)
+                .padding([.horizontal, .bottom])
+            Text("Line Width: \(lineWidth, specifier: "%.2f")")
         }
+        
     }
 }
 
