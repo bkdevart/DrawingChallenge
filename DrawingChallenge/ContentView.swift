b84337f806ee2bf5b1430a4bed01907f0e7d1dae
Brandon Knox <bknox.dev@gmail.com>
2021-04-10 15:02:45 -0700

    First challenge - drawing an arrow using rectangle and triangle shapes

--- DrawingChallenge/ContentView.swift
+++ DrawingChallenge/ContentView.swift
@@ -7,10 +7,42 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Rectangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Triangle()
+                .fill(Color.green)
+                .frame(width:300, height: 300)
+            Rectangle()
+                .fill(Color.green)
+                .frame(width:100, height: 300)
+        }
     }
 }
 
